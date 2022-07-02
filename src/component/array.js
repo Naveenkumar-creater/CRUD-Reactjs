3803b3495d7b8d26b2c809ffc4a76271eee2c6d2
NaveenKumar-creater <naveenmech1074@gmail.com>
2022-07-02 11:20:01 +0530

    first commit

---
+++ src/component/array.js
@@ -0,0 +1,25 @@
+
+const array=[
+
+    {  id:1,
+       Name:'Naveen',
+       Age:'23',
+       Dob:'05.04.1999',
+       Email:''
+    },
+    {  id:1,
+        Name:'Arun',
+        Age:'23',
+        Dob:'19.04.1999',
+       Email:""
+     },
+     {  id:1,
+        Name:'Vinoth',
+        Age:'23',
+        Dob:'27.04.1999',
+        Email:""
+     },
+  
+
+]
+export default array; 
